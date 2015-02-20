15fbc9956b47e18c57a0c4da14fae924c9e1acd2
dheidgerken <david.heidgerken@gmail.com>
2015-02-20 15:34:33 -0800

    commiting UserMgmt.business and .user packages
    usermgmt packages for updating databse

---
+++ src/UserMgmt/business/UserDelete.java
@@ -0,0 +1,33 @@
+package UserMgmt.business;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import UserMgmt.user.User;
+
+public class UserDelete extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+    
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String username= request.getParameter("username");
+		
+		UserDB dbConn = new UserDB();
+		
+		int i = dbConn.deleteUser(username);
+		if(i > 0){
+				response.sendRedirect("../index.jsp");
+		}else{
+			response.sendRedirect("signUpError.html");
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doPost(request, response);
+	}
+
+}

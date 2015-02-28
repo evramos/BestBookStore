c95dedd1f665e299baa6383a4165944afe34efe8
dheidgerken <david.heidgerken@gmail.com>
2015-02-28 15:42:49 -0800

    more edits to ratings

--- src/ratingMgmt/rating/RatingDB.java
+++ src/ratingMgmt/rating/RatingDB.java
@@ -1,5 +1,87 @@
 package ratingMgmt.rating;
 
-public class RatingDB {
+import java.sql.*;
+import java.util.*;
 
+import UserMgmt.user.User;
+import BookMgmt.book.book;
+import ratingMgmt.rating.rating;
+import data.dbConnect.DBConnectionPool;
+
+public class RatingDB {
+	final static String db_url ="jdbc:mysql://localhost:3306/bookstore";
+	final static String db_username ="root";
+	final static String db_passwd ="p0p1c0rn";
+	
+	DBConnectionPool connPool = null;
+	
+	public RatingDB()
+	{
+		this.connPool = setDBConnection();
+	}
+	
+	public DBConnectionPool setDBConnection()
+	{
+		try{
+			connPool = new DBConnectionPool(db_url, db_username, db_passwd);
+		}catch (Exception et) {
+			et.printStackTrace();
+		}
+		return connPool;
+	}
+	
+	public int addRating(rating Rating)
+	{
+		Statement stmt = null;
+		ResultSet rs = null;
+		int resultNo = 0;
+		Connection conn = null;
+		try{
+			conn = connPool.getConnection();
+			
+			if(conn != null){
+				stmt = conn.createStatement();
+				
+				String strQuery = "INSERT INTO `bookstore`.`ratings` (`Date`,`Stars`,`Comments`,`User_User ID`,`Book_Book ID`) VALUES (\""+rating.getDate()+"\",\""+rating.getStars()+"\", \""+rating.getComments()+"\", "+rating.getUserId()+", "+rating.getBookId()+")";
+				resultNo = stmt.executeUpdate(strQuery);
+			}
+		}catch(SQLException e){
+			for(Throwable t: e){	
+				t.printStackTrace();
+			}
+		}catch(Exception et) {
+			et.printStackTrace();
+		}finally {
+		    try {
+		    	if (rs != null){
+		            rs.close();
+		        }
+		    	if (stmt != null){
+		            stmt.close();
+		        }
+		        if (conn != null) {
+		            connPool.returnConnection(conn);
+		        }
+		    }catch(Exception e){
+		    	 System.err.println(e);
+		    }
+		}
+		return resultNo;
+	}
+	public int editRating(rating Rating)
+	{
+		
+	}
+	public int deleteRating(int RatingId)
+	{
+		
+	}
+	public rating selectRating(int RatingId)
+	{
+		
+	}
+	public ArrayList<rating> selectRatings()
+	{
+		
+	}
 }
--- src/ratingMgmt/rating/rating.java
+++ src/ratingMgmt/rating/rating.java
@@ -1,5 +1,71 @@
 package ratingMgmt.rating;
+import java.io.Serializable;
 
-public class rating {
+public class rating implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int ratingsId;
+	private String date;
+	private int stars;
+	private String comments;
+	private int userId;
+	private int bookId;
 
+	public rating(){}
+	public rating(String date, int stars, String comments, int userId, int bookId)
+	{
+		this.date = date;
+		this.stars = stars;
+		this.comments = comments;
+		this.userId = userId;
+		this.bookId = bookId;
+	}
+	public int getRatingsId()
+	{
+		return ratingsId;
+	}
+	public void setRatingsId(int ratingsId)
+	{
+		this.ratingsId = ratingsId;
+	}
+	public String getDate()
+	{
+		return date;
+	}
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	public int getStars()
+	{
+		return stars;
+	}
+	public void setStars(int stars)
+	{
+		this.stars = stars;
+	}
+	public String getComments()
+	{
+		return comments;
+	}
+	public void setComments(String comments)
+	{
+		this.comments = comments;
+	}
+	public int getUserId()
+	{
+		return userId;
+	}
+	public void setUserId(int userId)
+	{
+		this.userId = userId;
+	}
+	public int getbookId()
+	{
+		return bookId;
+	}
+	public void setBookId(int bookId)
+	{
+		this.bookId = bookId;
+	}
 }

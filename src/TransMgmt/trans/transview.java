d842130898fc4fa010582d2bb543cb12511f38a5
dheidgerken <david.heidgerken@gmail.com>
2015-03-07 12:25:28 -0800

    transview.java completed

--- src/TransMgmt/trans/transview.java
+++ src/TransMgmt/trans/transview.java
@@ -17,7 +17,48 @@ import java.io.Serializable;
 		private String category;
 		private float price;
 		private int invQty;
+
+		public transview(){}//only default constructor needed.
 		
+		//setters
+		public int getOrder(){return order;}
+		public void setOrder(int order){ this.order = order;}
+		
+		public int getTransDate(){return transDate;}
+		public void setTransDate(int transDate){this.transDate = transDate;}
+		
+		public int getUserId(){return userId;}
+		public void setUserId(int userId){this.userId = userId;}
+		
+		public String getLastName(){return lastName;}
+		public void setLastName(String lastName){this.lastName = lastName;}
+		
+		public String getFirstName(){return firstName;}
+		public void setFirstName(String firstName){this.firstName = firstName;}
+		
+		public String getEmail(){return email;}
+		public void setEmail(String email){ this.email = email;}
+		
+		public int getOrderQty(){ return orderQty;}
+		public void setOrderQty(int orderQty){ this.orderQty = orderQty;}
+		
+		public int getBookId(){return bookId;}
+		public void setBookId(int bookId){ this.bookId = bookId;}
+		
+		public String getTitle(){return title;}
+		public void setTitle(String title){ this.title = title;}
+		
+		public String getAuthor(){return author;}
+		public void setAuthor(String author){ this.author = author;}
+		
+		public String getCategory(){return category;}
+		public void setCategory(String category){ this.author = category;}
+		
+		public float getPrice(){return price;}
+		public void setPrice(float price){this.price = price;}
+		
+		public int getInvQty(){return invQty;}
+		publi void int setInvQty(int invQty){ this.invQty = invQty;}
 	}
 	
 

06fd55ce9943cd381c2fcd39051ebc0016e1ec73
dheidgerken <david.heidgerken@gmail.com>
2015-03-09 13:17:18 -0700

    sometransview changes

--- src/TransMgmt/trans/transview.java
+++ src/TransMgmt/trans/transview.java
@@ -8,16 +8,11 @@ import java.sql.Timestamp;
 		private int order;
 		private Timestamp transDate;
 		private int userId;
-		private String lastName;
-		private String firstName;
-		private String email;
 		private int orderQty;
 		private int bookId;
 		private String title;
-		private String author;
 		private String category;
 		private float price;
-		private int invQty;
 
 		public transview(){}//only default constructor needed.
 		
@@ -31,15 +26,6 @@ import java.sql.Timestamp;
 		public int getUserId(){return userId;}
 		public void setUserId(int userId){this.userId = userId;}
 		
-		public String getLastName(){return lastName;}
-		public void setLastName(String lastName){this.lastName = lastName;}
-		
-		public String getFirstName(){return firstName;}
-		public void setFirstName(String firstName){this.firstName = firstName;}
-		
-		public String getEmail(){return email;}
-		public void setEmail(String email){ this.email = email;}
-		
 		public int getOrderQty(){ return orderQty;}
 		public void setOrderQty(int orderQty){ this.orderQty = orderQty;}
 		
@@ -49,17 +35,11 @@ import java.sql.Timestamp;
 		public String getTitle(){return title;}
 		public void setTitle(String title){ this.title = title;}
 		
-		public String getAuthor(){return author;}
-		public void setAuthor(String author){ this.author = author;}
-		
 		public String getCategory(){return category;}
-		public void setCategory(String category){ this.author = category;}
+		public void setCategory(String category){ this.category = category;}
 		
 		public float getPrice(){return price;}
 		public void setPrice(float price){this.price = price;}
-		
-		public int getInvQty(){return invQty;}
-		public void setInvQty(int invQty){ this.invQty = invQty;}
 	}
 	
 

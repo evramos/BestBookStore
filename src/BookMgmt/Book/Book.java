8f691a1657e3f19f3e221fe27b9fe344b7899ae6
Matthew Ramos <evramos@uw.edu>
2015-03-10 22:32:31 -0700

    Change Book

--- src/BookMgmt/Book/Book.java
+++ src/BookMgmt/Book/Book.java
@@ -1,71 +1,44 @@
 package BookMgmt.Book;
 import java.io.Serializable;
-import java.sql.Blob;
 
 public class Book implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	
 	private int bookId;
+
 	private String title;
 	private String author;
-<<<<<<< HEAD
-	private Blob bookCoverArt;
-=======
 	private String category;
 	private String bookCoverPath;
->>>>>>> 8e7f5e2bae03de189905b0bb14dc6e7506771495
 	private String bookDescription;
 	private String edition;
+
 	private int year;
+
 	private String publisher;
-	private String category;
 	private String isbn10;
 	private String isbn13;
+
 	private float price;
 	private int invQty;
 	
 /*----------------------------------------------------------------------------*/
 	//Blank Constructor
 	public Book(){}
-	
 
 	//Default Constructor
-<<<<<<< HEAD
-	public Book(String title, String author, int year, String publisher,
-		String category, String isbn10, String isbn13, float price, int invQty)
-	{
-		this.title = title;
-		this.author = author;
-		this.year = year;
-		this.publisher = publisher;
-=======
 	public Book(String title, String author, String bookCoverPath, String category, String description,
 		String edition, int year, String publisher, String isbn10, String isbn13, float price, int invQty)
 	{
 		this.title = title;
 		this.author = author;
 		this.bookCoverPath = bookCoverPath;
->>>>>>> 8e7f5e2bae03de189905b0bb14dc6e7506771495
 		this.category = category;
-		this.isbn10 = isbn10;
-		this.isbn13 = isbn13;
-		this.price = price;
-		this.invQty = invQty;
-	}
-
-	//Book Everything Constructor
-	public Book(String title, String author, Blob art, String description, String edition, int year,
-		String publisher, String category, String isbn10, String isbn13, float price, int invQty)
-    {
-		this.title = title;
-		this.author = author;
-		bookCoverArt = art;
 		bookDescription = description;
 		this.edition = edition;
 		this.year = year;
 		this.publisher = publisher;
-		this.category = category;
 		this.isbn10 = isbn10;
 		this.isbn13 = isbn13;
 		this.price = price;
@@ -81,18 +54,12 @@ public class Book implements Serializable
 	
 	public String getAuthor() { return author; }
 	public void setAuthor(String author) { this.author = author; }
-<<<<<<< HEAD
-	
-	public Blob getBookCoverArt() { return bookCoverArt; }
-	public void setBookCoverArt(Blob art) { bookCoverArt = art; }
-=======
 
 	public String getCategory() { return category; }
 	public void setCategory(String category) { this.category = category; }
 
 	public String getBookCoverPath() { return bookCoverPath; }
 	public void setBookCoverPath(String path) { bookCoverPath = path; }
->>>>>>> 8e7f5e2bae03de189905b0bb14dc6e7506771495
 	
 	public String getBookDescription() { return bookDescription; }
 	public void setBookDescription(String description) { bookDescription = description; }
@@ -105,9 +72,6 @@ public class Book implements Serializable
 	
 	public String getPublisher() { return publisher; }
 	public void setPublisher(String publisher) { this.publisher = publisher; }
-	
-	public String getCategory() { return category; }
-	public void setCategory(String category) { this.category = category; }
 	
 	public String getIsbn10() { return isbn10; }
 	public void setIsbn10(String isbn10) { this.isbn10 = isbn10; }
